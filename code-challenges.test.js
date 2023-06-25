99f8dc9281ec4aa050de4c73aa7c23f056a4ca23
miguelosvaldo <miguel.o.molina@icloud.com>
2023-06-25 16:54:32 -0700

    week 6 done

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -14,7 +14,22 @@
 
 // a) Create a test with an expect statement using the variable provided.
 
-const hitchhikersCharacters = [
+describe("arrayWithSentence", () => {
+  it("returns an array with a sentence about each person with their name capitalized.", () => {
+    
+    const hitchhikersCharacters = [
+      { name: "ford prefect", occupation: "a hitchhiker" },
+      { name: "zaphod beeblebrox", occupation: "president of the galaxy" },
+      { name: "arthur dent", occupation: "a radio employee" }
+    ]
+    // Expected output: ["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."]
+    expect(arrayWithSentence(hitchhikersCharacters)).toEqual(["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."])
+  })
+})
+      //FIRST TEST = ReferenceError: arrayWithSentence is not defined
+      //SECOND TEST = 
+
+      const hitchhikersCharacters = [
   { name: "ford prefect", occupation: "a hitchhiker" },
   { name: "zaphod beeblebrox", occupation: "president of the galaxy" },
   { name: "arthur dent", occupation: "a radio employee" }
@@ -23,11 +38,43 @@ const hitchhikersCharacters = [
 
 // b) Create the function that makes the test pass.
 
+const arrayWithSentence = (parameter) => {
+  const firstLastNameCapital = parameter.map(value => {
+    [firstName, lastName] = value.name.split(" ")
+  const firstNameUpperCase = firstName.charAt(0).toUpperCase() + firstName.substring(1)
+  const lastNameUpperCase = lastName.charAt(0).toUpperCase() + lastName.substring(1)
+  const addOcuppation = value.occupation
+  return `${firstNameUpperCase} ${lastNameUpperCase} is ${addOcuppation}.`
+  })
+  return firstLastNameCapital
+}
+
+
 // Pseudo code:
+    //create function called arrayWithSentence that takes in one paramenter
+    //create a variable called firstLastNameCapital that will have the parameter mapped over
+    //going to iterate over array of objects 
+    //going to destructure array bu using a destructuring assignment in order to obtain first and last name seperately, will do so by using split(' ')which will seperate the first name and last name from the property
+    //going to declare a variable called lastNameUpperCase which will store the first letter of the first name  by accessing with charAt(0), and concat the rest of the substring from character 1. Going to repeat process for lastname.
+    //declare a varibale called addOcupation which will have the value of ocupation
+    //going to string intorpolate in order to receive a sentence and return that
+    //lastly return firstLastNameCapital array from the function
+
 
 // --------------------2) Create a function that takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3.
 
 // a) Create a test with an expect statement using the variables provided.
+
+describe("mixedDataArray", () => {
+  it("returns an array of only the REMAINDERS of the numbers when divided by 3.", () => {
+    const hodgepodge1 = [23, "Heyyyy!", 45, -10, 0, "Yo", false]
+      // Expected output: [ 2, 0, -1, 0 ]
+   expect(mixedDataArray(hodgepodge1)).toEqual([ 2, 0, -1, 0 ])
+   const hodgepodge2 = [5, "Hola", 43, -34, "greetings", true]
+   // Expected output: [ 2, 1, -1 ]
+   expect(mixedDataArray(hodgepodge2)).toEqual([ 2, 1, -1 ])
+  })
+})
 
 const hodgepodge1 = [23, "Heyyyy!", 45, -10, 0, "Yo", false]
 // Expected output: [ 2, 0, -1, 0 ]
@@ -36,11 +83,34 @@ const hodgepodge2 = [5, "Hola", 43, -34, "greetings", true]
 
 // b) Create the function that makes the test pass.
 
+const mixedDataArray = (param) => {
+  return param.filter(value => typeof value  === "number" )
+  .map(value => value % 3)
+}
+
 // Pseudo code:
+      //going to create function called mixedDataArray that will take one parameter
+      //going to filter over the parameter by using .filter() to return a new array of filtered values
+      //going to identify numbers only by using typeof operator 
+      //going to use .map() to iterate through the numbers and return the remainders by using the modulo operator when divided by 3
+
 
 // --------------------3) Create a function that takes in an array of numbers and returns the sum of all the numbers cubed.
 
 // a) Create a test with an expect statement using the variables provided.
+
+describe("arrOfNum", () => {
+  it("returns the sum of all the numbers cubed.", () => {
+    const cubeAndSum1 = [2, 3, 4]
+    // Expected output: 99
+   expect(arrOfNum(cubeAndSum1)).toEqual(99)
+   const cubeAndSum2 = [0, 5, 10]
+   // Expected output: 1125
+   expect(arrOfNum(cubeAndSum2)).toEqual(1125)
+  })
+})
+
+        // ReferenceError: arrOfNum is not defined
 
 const cubeAndSum1 = [2, 3, 4]
 // Expected output: 99
@@ -49,4 +119,17 @@ const cubeAndSum2 = [0, 5, 10]
 
 // b) Create the function that makes the test pass.
 
+const arrOfNum = (param) => {
+  let initialValue = 0
+  let cubedOfNums = param.map(value => value **3)
+  return cubedOfNums.reduce((acc, currentValue) =>
+    acc + currentValue, initialValue
+  )
+}
+
 // Pseudo code:
+
+        //going to crate function called arrOfNum that takes one parameter
+        //goint to declare a variable called initialValue and set it to 0 which will be the initial value
+        //going go use .map() to iterate over param which is an array, select each value and cube each element, then the cubed elements will be assigned to cubedOfNum array
+        //going to use .reduce() to iterate over each element of the new array, and accumulate them into the acc argument, then we add the accumulated elements to the current element and return that. 
--- rails-commenting.rb
+++ rails-commenting.rb
@@ -6,25 +6,25 @@
 
 # FILE: app/controller/blog_posts_controller.rb
 
-# ---1)
+# ---1)// Below is the controller, which is resposible for user interactions.
 class BlogPostsController < ApplicationController
   def index
-    # ---2)
+    # ---2) Below Blogs model is assigned to posts. Which are fetched by the index method.
     @posts = BlogPost.all
   end
 
-  # ---3)
+  # ---3) Below in the show method, BlogPost looks for a certain instance by using a param of a certain id and assigned it to posts.
   def show
     @post = BlogPost.find(params[:id])
   end
 
-  # ---4)
+  # ---4) Below a new user object is being made and assigned to the post instance.
   def new
     @post = BlogPost.new
   end
 
   def create
-    # ---5)
+    # ---5) A new user object is being created, will use blog_post_params to find necesssary paramenters. 
     @post = BlogPost.create(blog_post_params)
     if @post.valid?
       redirect_to blog_post_path(@post)
